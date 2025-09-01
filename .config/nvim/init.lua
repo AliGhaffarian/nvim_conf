b3f91c2f000b57b7c4676099926f5d1afda7d288
Ali Ghaffarian <alighaffarian9@gmail.com>
2025-09-01 14:41:06 +0330

    01_09_2025:14:41

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,57 +1,52 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" }, { out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.cmd([[
-  nnoremap <silent> K :call ShowDocumentation()<CR>
-  " Show hover when provider exists, fallback to vim's builtin behavior.
-  function! ShowDocumentation()
-    if CocAction('hasProvider', 'hover')
-      call CocActionAsync('definitionHover')
-    else
-      call feedkeys('K', 'in')
-    endif
-  endfunction
-]])
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = "\\"
-vim.g.maplocalleader = " "
+vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-	  "ervandew/supertab",
-	  "ThePrimeagen/vim-be-good",
-	  "neoclide/coc.nvim",
-	  "tpope/vim-surround",
-	  "nvim-lua/plenary.nvim",
-	  "nvim-telescope/telescope.nvim",
-          "nvim-treesitter/nvim-treesitter",
-	  "dhananjaylatkar/cscope_maps.nvim",
-	  "ibhagwan/fzf-lua", -- optional [for picker="fzf-lua"]
-	  "echasnovski/mini.pick", -- optional [for picker="mini-pick"]
-	  "folke/snacks.nvim", -- optional [for picker="snacks"]
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+	spec = {
+		"ThePrimeagen/vim-be-good",
+		"neoclide/coc.nvim",
+		"dhananjaylatkar/cscope_maps.nvim",
+		{
+			'nvim-telescope/telescope.nvim', tag = '0.1.8',
+			-- or                              , branch = '0.1.x',
+			dependencies = { 'nvim-lua/plenary.nvim' }
+		},
+		{
+			"kylechui/nvim-surround",
+			version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+			event = "VeryLazy",
+			config = function()
+				require("nvim-surround").setup({
+					-- Configuration here, or leave empty to use defaults
+				})
+			end
+		}
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "habamax" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
 require("cscope_maps").setup()
 
@@ -66,13 +61,17 @@ vim.cmd([[set number]])
 vim.cmd([[set number relativenumber]])
 vim.cmd([[set nu rnu]])
 
-
-vim.keymap.set('n', '<C-n>', vim.cmd.bnext, { desc = 'bnext' })
-vim.keymap.set('n', '<C-m>', vim.cmd.bprev, { desc = 'bprev' })
-
---telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- init.lua or inside lua config file
+vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true })
+-- NOTE: <C-;> is not recognized by most terminals.
+-- Instead, map ';' directly without Ctrl (optional)
+vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true }) -- or pick another convenient key
+
